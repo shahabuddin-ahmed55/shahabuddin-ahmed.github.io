5967f8ec4b5ddfda17ad77a2a35a62bc8eac3d96
Shahab Uddin Ahmed <shahabuddin2401026@gmail.com>
2021-09-04 19:28:32 +0500

    Something new

--- SYlani Project(Team creator)/apps/app.js
+++ SYlani Project(Team creator)/apps/app.js
@@ -1,24 +1,71 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+const firebaseConfig = {
+    apiKey: "AIzaSyBiFcFQd-MiM-4wIHyb_jK_fdFgIE_MQAg",
+    authDomain: "team-creater.firebaseapp.com",
+    projectId: "team-creater",
+    storageBucket: "team-creater.appspot.com",
+    messagingSenderId: "861396692052",
+    appId: "1:861396692052:web:9a4e92566bd920431391c1",
+    measurementId: "G-K0WWBHH7R9"
+  };
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyBQ9PhR5ycjwJgwCxUn8EgKQpKm79DKRf0",
-    authDomain: "team-creator-59dcb.firebaseapp.com",
-    projectId: "team-creator-59dcb",
-    storageBucket: "team-creator-59dcb.appspot.com",
-    messagingSenderId: "361878952490",
-    appId: "1:361878952490:web:c816b800373c058ecb0153",
-    measurementId: "G-NLZBWX2KHQ"
+   // Initialize Firebase
+   firebase.initializeApp(firebaseConfig);
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+      console.log(user);
+      document.getElementById("user").innerHTML = user.email;
+      document.getElementById("login_box").style.display = "none";
+      document.getElementById("welcome_box").style.display = "block";
+  } else {
+      document.getElementById("login_box").style.display = "block";
+      document.getElementById("welcome_box").style.display = "none";
   }
+});
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
-console.log("Shahabuddin");
+function login() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    console.log(email, password);
+
+
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            console.log(user);
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorMessage);
+            // ..
+        });
+}
+
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+    }).catch((error) => {
+        // An error happened.
+    });
+}
+
+function signup() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    console.log(email, password);
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // Signed in 
+            var user = userCredential.user;
+            // ...
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // ..
+        });
+} 
