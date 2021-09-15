ab78562df4b3ba8e92731d0a110e053e068c74c0
Shahab Uddin Ahmed <shahabuddin2401026@gmail.com>
2021-09-15 06:12:28 +0500

    upgrade

--- SYlani Project(Team creator)/apps/app.js
+++ SYlani Project(Team creator)/apps/app.js
@@ -1,60 +1,68 @@
 const login = () => {
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    console.log(email, password);
-    
-    
-    firebase.auth().signInWithEmailAndPassword(email, password)
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  console.log(email, password);
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
     .then((userCredential) => {
-        // Signed in 
-        window.location = 'creatTeam.html'
-        const user = userCredential.user;
-        console.log(user);
-        // ...
+      window.location = "creatTeam.html";
+      const user = userCredential.user;
     })
     .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorMessage);
-        // ..
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+      // ..
     });
-}
+};
 
 function logout() {
-    firebase.auth().signOut().then(() => {
-        // Sign-out successful.
-    }).catch((error) => {
-        // An error happened.
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      // Sign-out successful.
+    })
+    .catch((error) => {
+      // An error happened.
     });
 }
 
 function signup() {
-    const username = document.getElementById("username").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    console.log(username, email, password);
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then((userCredential) => {
-            // Signed in 
-            var user = userCredential.user;
-            window.location = 'creatTeam.html'
-            // ...
-        })
-        .catch((error) => {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // ..
-        });
+  const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      var user = userCredential.user;
+      window.location = "creatTeam.html";
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+    });
 }
 
 // TeamCreat Coodin
 
-function creat(){
+function creat() {
     const teamName = document.getElementById("teamName").value;
     const teamCategory = document.getElementById("teamCategory").value;
     const teamCategoryEmail = document.getElementById("teamCategoryEmail").value;
-    console.log(teamName, teamCategory, teamCategoryEmail)
-    window.location = 'team.html'
-    const element = document.getElementsByClassName("TeamsYouOwn");
-    element.innerHTML = "teamName <br /><hr /> teamCategory <br /><hr /> teamCategoryEmail";
-  }
+    const forDiv = document.getElementById("teamYouOwn");
+    const div = document.createElement('span');
+    window.location = "team.html";
+    div.appendChild(teamName);
+    div.appendChild(teamCategory);
+    div.appendChild(teamCategoryEmail);
+    const element = forDiv.appendChild(div);
+    console.log(div);
+}
+
+function skip() {
+  window.location = "team.html";
+}
