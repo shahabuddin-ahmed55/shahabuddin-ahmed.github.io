6c4732d223338bd4fdfe0d0b23b4a57393dbfd84
Shahab Uddin Ahmed <shahabuddin2401026@gmail.com>
2021-09-17 06:47:49 +0500

    upgrade

--- Firestore Chat App/app.js
+++ Firestore Chat App/app.js
@@ -1,115 +1,4 @@
 console.log("Shahabuddin");
-const firebaseConfig = {
-  apiKey: "AIzaSyAeJmgkkML1HzyHTatIdsiMacyZhlK1tvg",
-  authDomain: "chat-app-f8346.firebaseapp.com",
-  projectId: "chat-app-f8346",
-  storageBucket: "chat-app-f8346.appspot.com",
-  messagingSenderId: "48759623743",
-  appId: "1:48759623743:web:7f119ea10628730e3a688a",
-  measurementId: "G-BKDPQXWGQH",
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const firestore = firebase.firestore();
-
-function login() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password");
-    auth.signInWithEmailAndPassword(email, password)
-    .catch((error) => {
-        const errorMessage = error.message;
-        console.log(errorMessage);
-    });
-
-}
-
-function logout(){
-    auth.signOut();
-}
-
-function signin(){
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    auth.createUserWithEmailAndPassword(email, password)
-    .catch((error) => {
-        const errorMessage = error.message;
-        console.log(errorMessage);
-    });
-
-}
-
-auth.onAuthStateChanged((user) => {
-    if(user) {
-        firestore.collection('user').doc('user.uid').set({
-            email: user.email,
-            lastLoggedInAt: new Date()
-        })
-            .then(() => {
-                console.log("Documnet written");
-            })
-            .catch((error) => {
-                console.log("Error adding document: ", error);
-            });
-            setData(user);
-            setMessages();
-            document.getElementById("user").innerHTML = user.email;
-            document.getElementById("login").style.display = "none";
-            document.getElementById("afterLogInBox").style.display = "block";
-    } else{
-        document.getElementById("login").style.display = "block";
-        document.getElementById("afterLogInBox").style.display = "none";
-    }
-});
-
-const setData = (user) => {
-    firestore.collection('user').doc(user.uid).get().then((querySnapshot) => {
-        const data = querySnapshot.data();
-        const lastLoggedInAt = data.lastLoggedInAt;
-        const lastLoggedInSpan = document.getElementById("lastLoggedIn");
-        lastLoggedInSpan.innerHTML = lastLoggedInAt;
-    })
-}
-
-const setMessages = () => {
-    const messagesRef = firestore.collection('messages');
-    messagesRef.onSnapshot((snapshot) => {
-        snapshot.docChanges().forEach(function (change) {
-            if (change.type === "added") {
-                creatElementsForMessages(change.doc.data());
-            }
-        })
-    })
-}
-
-const sendMessage = () => {
-    const user = auth.currentUser.email;
-    const message = document.getElementById("message").value;
-    firestore.collection('messages').add({
-        user, message, time: Date.now()
-    }).then((docRef) => {
-        console.log("Document written with ID: ", docRef.id);
-    })
-    .catch((error) => {
-        console.error("Error adding document: ", error);
-    })
-}
-
-const createElementsForMessage = (childData) => {
-    const messagesDiv = document.getElementById("messages");
-    const childDiv = document.createElement('div');
-    childDiv.classList.add("single-message-body");
-    const messageTextDiv = document.createElement('div');
-    messageTextDiv.classList.add("messages-text");
-    const senderDiv = document.createElement('div');
-    senderDiv.classList.add("message-sender");
-    messageTextDiv.innerHTML = childData.message;
-    const userEmail = auth.currentUser.email;
-    if (childData.user === userEmail) {
-        senderDiv.innerHTML = 
-    }
-}
 // const firebaseConfig = {
 //   apiKey: "AIzaSyAeJmgkkML1HzyHTatIdsiMacyZhlK1tvg",
 //   authDomain: "chat-app-f8346.firebaseapp.com",
@@ -117,7 +6,8 @@ const createElementsForMessage = (childData) => {
 //   storageBucket: "chat-app-f8346.appspot.com",
 //   messagingSenderId: "48759623743",
 //   appId: "1:48759623743:web:7f119ea10628730e3a688a",
-//   measurementId: "G-BKDPQXWGQH",};
+//   measurementId: "G-BKDPQXWGQH",
+// };
 
 // // Initialize Firebase
 // firebase.initializeApp(firebaseConfig);
@@ -125,61 +15,61 @@ const createElementsForMessage = (childData) => {
 // const firestore = firebase.firestore();
 
 // function login() {
-//     const email = document.getElementById("email").value;
-//     const password = document.getElementById("password").value;
+//   const email = document.getElementById("email").value;
+//   const password = document.getElementById("password");
 //     auth.signInWithEmailAndPassword(email, password)
-//         .catch((error) => {
-//             const errorMessage = error.message;
-//             console.log(errorMessage);
-//         });
+//     .catch((error) => {
+//         const errorMessage = error.message;
+//         console.log(errorMessage);
+//     });
+
 // }
 
-// function logout() {
+// function logout(){
 //     auth.signOut();
 // }
 
-// function signin() {
+// function signin(){
 //     const email = document.getElementById("email").value;
 //     const password = document.getElementById("password").value;
-//     console.log(email, password);
 //     auth.createUserWithEmailAndPassword(email, password)
-//         .catch((error) => {
-//             const errorMessage = error.message;
-//             console.log(errorMessage);
-//         });
+//     .catch((error) => {
+//         const errorMessage = error.message;
+//         console.log(errorMessage);
+//     });
+
 // }
 
-
 // auth.onAuthStateChanged((user) => {
-//     if (user) {
-//         firestore.collection('users').doc(user.uid).set({
+//     if(user) {
+//         firestore.collection('user').doc('user.uid').set({
 //             email: user.email,
 //             lastLoggedInAt: new Date()
 //         })
 //             .then(() => {
-//                 console.log("Document written");
+//                 console.log("Documnet written");
 //             })
 //             .catch((error) => {
-//                 console.error("Error adding document: ", error);
+//                 console.log("Error adding document: ", error);
 //             });
-//         setData(user);
-//         setMessages();
-//         document.getElementById("user").innerHTML = user.email;
-//         document.getElementById("login").style.display = "none";
-//         document.getElementById("afterLogInBox").style.display = "block";
-//     } else {
+//             setData(user);
+//             setMessages();
+//             document.getElementById("user").innerHTML = user.email;
+//             document.getElementById("login").style.display = "none";
+//             document.getElementById("afterLogInBox").style.display = "block";
+//     } else{
 //         document.getElementById("login").style.display = "block";
 //         document.getElementById("afterLogInBox").style.display = "none";
 //     }
 // });
 
 // const setData = (user) => {
-//     firestore.collection('users').doc(user.uid).get().then((querySnapshot) => {
+//     firestore.collection('user').doc(user.uid).get().then((querySnapshot) => {
 //         const data = querySnapshot.data();
 //         const lastLoggedInAt = data.lastLoggedInAt;
 //         const lastLoggedInSpan = document.getElementById("lastLoggedIn");
 //         lastLoggedInSpan.innerHTML = lastLoggedInAt;
-//     });
+//     })
 // }
 
 // const setMessages = () => {
@@ -187,11 +77,11 @@ const createElementsForMessage = (childData) => {
 //     messagesRef.onSnapshot((snapshot) => {
 //         snapshot.docChanges().forEach(function (change) {
 //             if (change.type === "added") {
-//                 createElementsForMessage(change.doc.data());
+//                 creatElementsForMessages(change.doc.data());
 //             }
-//         });
-//     });
-// };
+//         })
+//     })
+// }
 
 // const sendMessage = () => {
 //     const user = auth.currentUser.email;
@@ -201,29 +91,147 @@ const createElementsForMessage = (childData) => {
 //     }).then((docRef) => {
 //         console.log("Document written with ID: ", docRef.id);
 //     })
-//         .catch((error) => {
-//             console.error("Error adding document: ", error);
-//         });
-// };
+//     .catch((error) => {
+//         console.error("Error adding document: ", error);
+//     })
+// }
 
 // const createElementsForMessage = (childData) => {
 //     const messagesDiv = document.getElementById("messages");
 //     const childDiv = document.createElement('div');
 //     childDiv.classList.add("single-message-body");
 //     const messageTextDiv = document.createElement('div');
-//     messageTextDiv.classList.add("message-text");
+//     messageTextDiv.classList.add("messages-text");
 //     const senderDiv = document.createElement('div');
 //     senderDiv.classList.add("message-sender");
 //     messageTextDiv.innerHTML = childData.message;
 //     const userEmail = auth.currentUser.email;
 //     if (childData.user === userEmail) {
 //         senderDiv.innerHTML = "You";
-//         childDiv.classList.add("sender");
-//     } else {
+//         childDiv.classList.add("sender")
+//     } else{
 //         senderDiv.innerHTML = childData.user;
 //         childDiv.classList.add("reciever");
 //     }
 //     childDiv.appendChild(senderDiv);
 //     childDiv.appendChild(messageTextDiv);
 //     messagesDiv.appendChild(childDiv);
-// };
+// }
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAeJmgkkML1HzyHTatIdsiMacyZhlK1tvg",
+  authDomain: "chat-app-f8346.firebaseapp.com",
+  projectId: "chat-app-f8346",
+  storageBucket: "chat-app-f8346.appspot.com",
+  messagingSenderId: "48759623743",
+  appId: "1:48759623743:web:7f119ea10628730e3a688a",
+  measurementId: "G-BKDPQXWGQH",};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+function login() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    auth.signInWithEmailAndPassword(email, password)
+        .catch((error) => {
+            const errorMessage = error.message;
+            console.log(errorMessage);
+        });
+}
+
+function logout() {
+    auth.signOut();
+}
+
+function signin() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    console.log(email, password);
+    auth.createUserWithEmailAndPassword(email, password)
+        .catch((error) => {
+            const errorMessage = error.message;
+            console.log(errorMessage);
+        });
+}
+
+
+auth.onAuthStateChanged((user) => {
+    if (user) {
+        firestore.collection('users').doc(user.uid).set({
+            email: user.email,
+            lastLoggedInAt: new Date()
+        })
+            .then(() => {
+                console.log("Document written");
+            })
+            .catch((error) => {
+                console.error("Error adding document: ", error);
+            });
+        setData(user);
+        setMessages();
+        document.getElementById("user").innerHTML = user.email;
+        document.getElementById("login").style.display = "none";
+        document.getElementById("afterLogInBox").style.display = "block";
+    } else {
+        document.getElementById("login").style.display = "block";
+        document.getElementById("afterLogInBox").style.display = "none";
+    }
+});
+
+const setData = (user) => {
+    firestore.collection('users').doc(user.uid).get().then((querySnapshot) => {
+        const data = querySnapshot.data();
+        const lastLoggedInAt = data.lastLoggedInAt.toDate();
+        const lastLoggedInSpan = document.getElementById("lastLoggedIn");
+        lastLoggedInSpan.innerHTML = lastLoggedInAt;
+    });
+}
+
+const setMessages = () => {
+    const messagesRef = firestore.collection('messages');
+    messagesRef.onSnapshot((snapshot) => {
+        snapshot.docChanges().forEach(function (change) {
+            if (change.type === "added") {
+                createElementsForMessage(change.doc.data());
+            }
+        });
+    });
+};
+
+const sendMessage = () => {
+    const user = auth.currentUser.email;
+    const message = document.getElementById("message").value;
+    firestore.collection('messages').add({
+        user, message, time: Date.now()
+    }).then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+    })
+        .catch((error) => {
+            console.error("Error adding document: ", error);
+        });
+};
+
+const createElementsForMessage = (childData) => {
+    const messagesDiv = document.getElementById("messages");
+    const childDiv = document.createElement('div');
+    childDiv.classList.add("single-message-body");
+    const messageTextDiv = document.createElement('div');
+    messageTextDiv.classList.add("message-text");
+    const senderDiv = document.createElement('div');
+    senderDiv.classList.add("message-sender");
+    messageTextDiv.innerHTML = childData.message;
+    const userEmail = auth.currentUser.email;
+    if (childData.user === userEmail) {
+        senderDiv.innerHTML = "You";
+        childDiv.classList.add("sender");
+    } else {
+        senderDiv.innerHTML = childData.user;
+        childDiv.classList.add("reciever");
+    }
+    childDiv.appendChild(senderDiv);
+    childDiv.appendChild(messageTextDiv);
+    messagesDiv.appendChild(childDiv);
+};
